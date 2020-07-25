faae60e6c3798a32cf9980475b97a3cae549ab59
jorge-17 <ja.rodartemartinez@gmail.com>
2020-07-25 00:36:09 -0500

    Se agragan las vistas de listas de camaras y localidades | v0.2

--- DistSocial/DistSocialApp/urls.py
+++ DistSocial/DistSocialApp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/camaras/', views.camaras, name='camaras'),
-    path('/localidades/', views.localidades, name='localidades')
+    path('camaras/', views.camaras, name='camaras'),
+    path('localidades/', views.localidades, name='localidades')
 ]
--- DistSocial/DistSocialApp/views.py
+++ DistSocial/DistSocialApp/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import CatCamaras, CatLocalidades
 
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hola, estan en el index de la app de distancia social")
 
-def camaras(request):
-    response = "You're looking at the camaras"
-    return HttpResponse(response)
-
 def localidades(request):
-    response = "You're looking at the localidades"
-    return HttpResponse(response)
+    lista_localidades = CatLocalidades.objects.order_by('id_CatLocalidad')[:15]
+    template = loader.get_template('DistSocialApp/localidades.html')
+    context = {
+        'lista_localidades': lista_localidades
+    }
+    
+    return HttpResponse(template.render(context, request))
+
+def camaras(request):
+    lista_camaras = CatCamaras.objects.order_by('id_CatCamara')[:15]
+    template = loader.get_template('DistSocialApp/camaras.html')
+    context = {
+        'lista_camaras': lista_camaras
+    }
+    return HttpResponse(template.render(context, request))
